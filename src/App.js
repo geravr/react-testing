651358ad685c2742f2247f0d94c2823bd209bf3c
Gerardo Villa <gera.villa21@gmail.com>
2020-11-03 08:33:35 -0600

    se actualiza estructura de componentes hijos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import style from "./app.module.css";
+
+// Components
+import TodoForm from "./components/TodoForm/TodoForm";
+import Todo from "./components/Todo/Todo"; 
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: "Todo 1",
+      isCompleted: false
+    },
+    {
+      text: "Todo 2",
+      isCompleted: false
+    },
+    {
+      text: "Todo 3",
+      isCompleted: false
+    }
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [{ text }, ...todos];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={style.app}>
+      <div className={style.todoList}>
+        <TodoForm addTodo={addTodo} />
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+          />
+        ))}
+      </div>
     </div>
   );
 }
